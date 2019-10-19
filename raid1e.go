7141f63460014e69b8b275b98e9915bd155471fd
AlexTeoKim051292 <alrkim@nu.edu.kz>
2019-10-19 15:34:59 +0600

    Raid 1e

---
+++ raid1e.go
@@ -0,0 +1,31 @@
+package student
+
+import "github.com/01-edu/z01"
+
+func Raid1e(x, y int) {
+	if x <= 0 || y <= 0 {
+	} else {
+		for j := 0; j < y; j++ {
+			for i := 0; i < x; i++ {
+				if i == 0 && j == 0 {
+					z01.PrintRune(65)
+				} else if i == 0 && j == y-1 {
+					z01.PrintRune(67)
+				} else if i == x-1 && j == 0 {
+					z01.PrintRune(67)
+				} else if i == x-1 && j == y-1 {
+					z01.PrintRune(65)
+				} else if j == 0 || j == y-1 {
+					z01.PrintRune(66)
+				} else if i == 0 || i == x-1 {
+					z01.PrintRune(66)
+				} else {
+					z01.PrintRune(32)
+				}
+
+			}
+			z01.PrintRune(10)
+		}
+	}
+
+}
