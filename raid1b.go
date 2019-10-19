ff1f94b36726b9400ada6702272541e05e2ae300
AlexTeoKim051292 <alrkim@nu.edu.kz>
2019-10-19 15:19:27 +0600

    Raid 1b

--- raid1b.go
+++ raid1b.go
@@ -8,17 +8,17 @@ func Raid1a(x, y int) {
                 for j := 0; j < y; j++ {
                         for i := 0; i < x; i++ {
                                 if i == 0 && j == 0 {
-                                        z01.PrintRune('/')
+                                        z01.PrintRune(47)
                                 } else if i == 0 && j == y-1 {
-                                        z01.PrintRune('\')
+                                        z01.PrintRune(92)
                                 } else if i == x-1 && j == 0 {
-                                        z01.PrintRune('\')
+                                        z01.PrintRune(92)
                                 } else if i == x-1 && j == y-1 {
-                                        z01.PrintRune('/')
+                                        z01.PrintRune(47)
                                 } else if j == 0 || j == y-1 {
-                                        z01.PrintRune('*')
+                                        z01.PrintRune(42)
                                 } else if i == 0 || i == x-1 {
-                                        z01.PrintRune('*')
+                                        z01.PrintRune(42)
                                 } else {
                                         z01.PrintRune(32)
                                 }
