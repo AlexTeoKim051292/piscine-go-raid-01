14ac6c12aa7d6318be231ef39ce34c411a57692d
AlexTeoKim051292 <alrkim@nu.edu.kz>
2019-10-19 15:22:48 +0600

    Raid 1b

--- raid1b.go
+++ raid1b.go
@@ -2,31 +2,30 @@ package student
 
 import "github.com/01-edu/z01"
 
-func Raid1a(x, y int) {
-        if x <= 0 || y <= 0 {
-        } else {
-                for j := 0; j < y; j++ {
-                        for i := 0; i < x; i++ {
-                                if i == 0 && j == 0 {
-                                        z01.PrintRune(47)
-                                } else if i == 0 && j == y-1 {
-                                        z01.PrintRune(92)
-                                } else if i == x-1 && j == 0 {
-                                        z01.PrintRune(92)
-                                } else if i == x-1 && j == y-1 {
-                                        z01.PrintRune(47)
-                                } else if j == 0 || j == y-1 {
-                                        z01.PrintRune(42)
-                                } else if i == 0 || i == x-1 {
-                                        z01.PrintRune(42)
-                                } else {
-                                        z01.PrintRune(32)
-                                }
+func Raid1b(x, y int) {
+	if x <= 0 || y <= 0 {
+	} else {
+		for j := 0; j < y; j++ {
+			for i := 0; i < x; i++ {
+				if i == 0 && j == 0 {
+					z01.PrintRune(47)
+				} else if i == 0 && j == y-1 {
+					z01.PrintRune(92)
+				} else if i == x-1 && j == 0 {
+					z01.PrintRune(92)
+				} else if i == x-1 && j == y-1 {
+					z01.PrintRune(47)
+				} else if j == 0 || j == y-1 {
+					z01.PrintRune(42)
+				} else if i == 0 || i == x-1 {
+					z01.PrintRune(42)
+				} else {
+					z01.PrintRune(32)
+				}
 
-                        }
-                        z01.PrintRune(10)
-                }
-        }
+			}
+			z01.PrintRune(10)
+		}
+	}
 
 }
-
